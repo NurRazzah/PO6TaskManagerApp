9aea46ae755b02a283ac653c0559bb6fa8498c29
16022738 <azzah1706@gmail.com>
2018-05-28 14:18:00 +0800

    done

--- app/src/main/java/com/example/a16022738/po6taskmanagerapp/DBHelper.java
+++ app/src/main/java/com/example/a16022738/po6taskmanagerapp/DBHelper.java
@@ -1,8 +1,12 @@
 package com.example.a16022738.po6taskmanagerapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -14,7 +18,6 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String COLUMN_DESCRIPTION = "description";
 
 
-
     public DBHelper(Context context) {
         super(context, DATABASENAME, null, DATABASE_VERSION);
     }
@@ -22,7 +25,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_TASK + "("
-                + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COLUMN_NAME + " TEXT, "
                 + COLUMN_DESCRIPTION + " TEXT )";
         db.execSQL(createTable);
@@ -31,8 +34,42 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_TASK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
         onCreate(db);
+
+    }
+
+    public ArrayList<Task> getAllTasks() {
+        ArrayList<Task> tasks = new ArrayList<Task>();
+
+        String selectQuery = "SELECT " + COLUMN_ID + "," + COLUMN_NAME + "," + COLUMN_DESCRIPTION + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String desc = cursor.getString(2);
+
+                Task getT = new Task(id, name, desc);
+                tasks.add(getT);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+
+    public void insertTask(String name, String desc){
+        SQLiteDatabase dbase = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_DESCRIPTION, desc);
+        dbase.insert(TABLE_TASK, null, values);
+        dbase.close();
 
     }
 }
--- app/src/main/java/com/example/a16022738/po6taskmanagerapp/MainActivity.java
+++ app/src/main/java/com/example/a16022738/po6taskmanagerapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.a16022738.po6taskmanagerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnAdd;
     ListView lvTask;
+    ArrayList<Task> al;
+    ArrayList<String> alString;
+    ArrayAdapter aa;
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
         btnAdd = (Button)findViewById(R.id.btnAdd);
         lvTask = (ListView)findViewById(R.id.lvTask);
+        db = new DBHelper(this);
+        db.getWritableDatabase();
+
+        al = db.getAllTasks();
+        db.close();
+
+        alString = new ArrayList<>();
+        for (int i= 0; i < al.size(); i++){
+            String currentTaskString = al.get(i).getName();
+            currentTaskString += "\n" + al.get(i).getDescription();
+            alString.add(currentTaskString);
+        }
+
+        aa = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, alString);
+
+        lvTask.setAdapter(aa);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddTask.class);
+                startActivityForResult(i, 9);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 9){
+            lvTask.performClick();
+        }
     }
 }
